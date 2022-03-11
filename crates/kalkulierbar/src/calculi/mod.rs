17ee64dac8a85befd11198598553c2eb9c704649
Daniel <d.drodt@gmx.de>
2022-03-11 19:10:48 +0100

    Make CalculusKind serializable

--- crates/kalkulierbar/src/calculi/mod.rs
+++ crates/kalkulierbar/src/calculi/mod.rs
@@ -1,11 +1,15 @@
 use std::convert::TryFrom;
 use std::fmt;
 
+use serde::{Deserialize, Serialize};
+
 pub mod dpll;
 pub mod resolution;
 pub mod sequent;
 pub mod tableaux;
 
+#[derive(Debug, Deserialize, Clone, Copy, PartialEq, Eq)]
+#[serde(try_from = "&str")]
 pub enum CalculusKind {
     PropTableaux,
     FOTableaux,
@@ -13,22 +17,28 @@ pub enum CalculusKind {
     FOResolution,
     DPLL,
     NCTableaux,
+    PropSequent,
+    FOSequent,
+}
+
+impl CalculusKind {
+    pub fn to_str(&self) -> &'static str {
+        match self {
+            CalculusKind::PropTableaux => "prop-tableaux",
+            CalculusKind::FOTableaux => "fo-tableaux",
+            CalculusKind::PropResolution => "prop-resolution",
+            CalculusKind::FOResolution => "fo-resolution",
+            CalculusKind::DPLL => "dpll",
+            CalculusKind::NCTableaux => "nc-tableaux",
+            CalculusKind::PropSequent => "prop-sequent",
+            CalculusKind::FOSequent => "fo-sequent",
+        }
+    }
 }
 
 impl fmt::Display for CalculusKind {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                CalculusKind::PropTableaux => "prop-tableaux",
-                CalculusKind::FOTableaux => "fo-tableaux",
-                CalculusKind::PropResolution => "prop-resolution",
-                CalculusKind::FOResolution => "fo-resolution",
-                CalculusKind::DPLL => "dpll",
-                CalculusKind::NCTableaux => "nc-tableaux",
-            }
-        )
+        write!(f, "{}", self.to_str())
     }
 }
 
@@ -43,7 +53,18 @@ impl<'a> TryFrom<&'a str> for CalculusKind {
             "fo-resolution" => Ok(CalculusKind::FOResolution),
             "dpll" => Ok(CalculusKind::DPLL),
             "nc-tableaux" => Ok(CalculusKind::NCTableaux),
+            "prop-sequent" => Ok(CalculusKind::PropSequent),
+            "fo-sequent" => Ok(CalculusKind::FOSequent),
             _ => Err("Unknown calculus"),
         }
+    }
+}
+
+impl Serialize for CalculusKind {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(self.to_str())
     }
 }
